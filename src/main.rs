3e278d6e25eca7256183a1368c6fc6fcb27ebce9
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-06-16 11:35:40 +0200

    Add main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!")
+use simple::simple_lexer::SimpleLexer;
+use simple::simple_parser::SimpleParser;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = std::env::args().nth(1).expect("no file given");
+    let program = std::fs::read_to_string(file)?;
+    let lexer = SimpleLexer::default();
+    let parser = SimpleParser::new(Box::new(lexer));
+    let ast = parser.parse_program(&program)?;
+
+    println!("{:?}", ast);
+
+    Ok(())
 }
